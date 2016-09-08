3f3b76b76f69589d36a646c4f42a654f6021aafc
João Paulo Figueira <joao.figueira@frotcom.com>
2016-09-08 13:44:08 +0100

    Streamlined Scala code.

--- scala/src/ServerLog.scala
+++ scala/src/ServerLog.scala
@@ -17,33 +17,21 @@ object ServerLog {
   }
 }
 
-
-class LogRecord(val period: Int, val value: Double) { 
-}
-
+class LogRecord(val period: Int, val value: Double)
 
 class WeeklyLogModel(val radius: Double) {
-  private var models: Array[LinearRegressionModel] = new Array[LinearRegressionModel](2016) 
+  private var models = new Array[LinearRegressionModel](2016) 
   
-  for(i <- 0 to (models.length - 1)) {
+  for(i <- 0 to (models.length - 1))
     models(i) = new LinearRegressionModel
-  }
   
-  def update(record: LogRecord) {
-    models(getIndex(record)).update(record.period, record.value)
-  }
+  def update(record: LogRecord) = models(getIndex(record)).update(record.period, record.value)
   
-  def isOutlier(record: LogRecord): Boolean = {
-    models(getIndex(record)).isOutlier(radius, record.period, record.value)
-  }
+  def isOutlier(record: LogRecord): Boolean = models(getIndex(record)).isOutlier(radius, record.period, record.value)
   
-  def getValue(record: LogRecord): Double = {
-    models(getIndex(record)).getValue(record.period)
-  }
+  def getValue(record: LogRecord): Double = models(getIndex(record)).getValue(record.period)
   
-  private def getIndex(record: LogRecord): Int = {
-    record.period % 2016
-  }
+  private def getIndex(record: LogRecord): Int = record.period % 2016
 }
 
 class LinearRegressionModel {
@@ -73,9 +61,7 @@ class LinearRegressionModel {
     alpha = meanY - beta * meanX
   }
   
-  def getValue(x: Double): Double = {
-    alpha + beta * x
-  }
+  def getValue(x: Double): Double = alpha + beta * x
   
   def isOutlier(radius: Double, x: Double, y: Double): Boolean = {
     val yHat = alpha + beta * x
@@ -85,11 +71,7 @@ class LinearRegressionModel {
   
   // Private stuff
   
-  private def getResidualSumOfSquares(): Double = {
-    beta * beta * sumXX - 2 * beta * sumXY + 2 * alpha * beta * sumX + sumYY - 2 * alpha * sumY + alpha * alpha * count
-  }
+  private def getResidualSumOfSquares(): Double = beta * beta * sumXX - 2 * beta * sumXY + 2 * alpha * beta * sumX + sumYY - 2 * alpha * sumY + alpha * alpha * count
   
-  private def getResidualVariance(): Double = {
-    getResidualSumOfSquares / (count - 2)
-  }
+  private def getResidualVariance(): Double = getResidualSumOfSquares / (count - 2)
 }
