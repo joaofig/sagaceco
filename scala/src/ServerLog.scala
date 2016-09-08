1d3392356629485d03ee0104e27f40d41afe6734
João Paulo Figueira <joao.figueira@frotcom.com>
2016-09-08 12:33:26 +0100

    Final version of the Scala ServerLog example.

--- scala/src/ServerLog.scala
+++ scala/src/ServerLog.scala
@@ -1,27 +1,48 @@
 // Server log sample
 
+import scala.io.Source
+
 object ServerLog {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val weeklyModel = new WeeklyLogModel(3.2)
+    
+    for(line <- Source.fromFile("server-log-bad.csv").getLines().drop(1)) {
+      val pair = line.split(",")
+      val record = new LogRecord(pair(0).toInt, pair(1).toDouble)
+      
+      if (record.period > 32 * 2016 && weeklyModel.isOutlier(record))
+        println("Outlier " + record.period + " - " + record.value + " (" + weeklyModel.getValue(record) + ")")
+      weeklyModel.update(record)
+    }
   }
 }
 
-class LogRecord {
-  
+
+class LogRecord(val period: Int, val value: Double) { 
 }
 
-class WeeklyLogModel(radius: Double) {
+
+class WeeklyLogModel(val radius: Double) {
+  private var models: Array[LinearRegressionModel] = new Array[LinearRegressionModel](2016) 
+  
+  for(i <- 0 to (models.length - 1)) {
+    models(i) = new LinearRegressionModel
+  }
   
   def update(record: LogRecord) {
-    
+    models(getIndex(record)).update(record.period, record.value)
   }
   
   def isOutlier(record: LogRecord): Boolean = {
-    false
+    models(getIndex(record)).isOutlier(radius, record.period, record.value)
   }
   
   def getValue(record: LogRecord): Double = {
-    0.0
+    models(getIndex(record)).getValue(record.period)
+  }
+  
+  private def getIndex(record: LogRecord): Int = {
+    record.period % 2016
   }
 }
 
